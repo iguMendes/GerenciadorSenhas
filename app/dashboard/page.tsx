61130c5f766c5ed6bd0d93ac14cc0531bd21ae98
igor pc <igorast113@gmail.com>
2025-03-22 12:01:23 -0300

    commi na dev

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function Dashboard() {
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <h1 className="text-2xl font-bold">Bem-vindo à sua Dashboard</h1>
+        {/* Aqui você pode adicionar os recursos do seu aplicativo */}
+      </div>
+    );
+  }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+import { FaLock } from "react-icons/fa6";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "./firebase/config";
+import { useRouter } from "next/router";
+
 export default function LoginPage() {
-    return (
-      <div className="flex min-h-screen items-center justify-center">
-        <h1 className="text-2xl font-bold">Login</h1>
-      </div>
-    );
-  }
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log("Usuário logado:", result.user);
+      router.push("/dashboard");
+    } catch (error) {
+      console.error("Erro no login:", error);
+    }
+  };
+
+  return (
+    <div className="flex min-h-screen items-center justify-center border">
+      <button 
+        onClick={handleLogin} 
+        className="px-4 py-2 bg-blue-500 text-white rounded-md cursor-pointer hover:bg-blue-600 hover:scale-105 transition-all duration-300"
+      >
+        <FaLock className="text-blue-100 text-4xl translate-x-12 transition-all duration-300" />
+        Entrar com google
+      </button>
+    </div>
+  );
+}
